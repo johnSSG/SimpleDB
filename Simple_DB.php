4536c3cc9fb5f422c4a8b96bdeee856957847bc4
John <jfairfull@simplicitysolutionsgroup.com>
2015-07-31 02:14:07 -0400

    Update Simple_DB.php

--- Simple_DB.php
+++ Simple_DB.php
@@ -85,48 +85,14 @@ class Simple_DB {
 		return $output;
 	}
 	
-	private function indent($json) {
-	    $result      = '';
-	    $pos         = 0;
-	    $strLen      = strlen($json);
-    	$indentStr   = '  ';
-	    $newLine     = "\n";
-	    $prevChar    = '';
-	    $outOfQuotes = true;
-	    for ($i=0; $i<=$strLen; $i++) {
-	        $char = substr($json, $i, 1);
-	        if ($char == '"' && $prevChar != '\\') {
-	            $outOfQuotes = !$outOfQuotes;
-	        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
-	            $result .= $newLine;
-	            $pos --;
-	            for ($j=0; $j<$pos; $j++) {
-	                $result .= $indentStr;
-	            }
-	        }
-	        $result .= $char;
-	        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-	            $result .= $newLine;
-	            if ($char == '{' || $char == '[') {
-	                $pos ++;
-    	       }
-	           for ($j = 0; $j < $pos; $j++) {
-    	           $result .= $indentStr;
-        	   }
-	        }
-    	    $prevChar = $char;
-	    }
-		return $result;		
-	}
-	
 	public function post($c) {
 		$file = $this->check(uniqid());
-		$put  = file_put_contents($this->dir.$this->type.$file.'.json', $this->indent(json_encode((object) $c)));
+		$put  = file_put_contents($this->dir.$this->type.$file.'.json', json_encode((object) $c, JSON_PRETTY_PRINT));
 		return ($put ? $file : false);
 	}
 	
 	public function put($id, $c) {
-		$content = $this->indent(json_encode((object) $c));
+		$content = json_encode((object) $c, JSON_PRETTY_PRINT);
 		$file    = str_replace('.json', '', $id).'.json';
 		$put     = file_put_contents($this->dir.$this->type.$file, $content);
 		return ($put ? (object) $c : false);
